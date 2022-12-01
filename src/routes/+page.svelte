be56cbaf9944eb31221e9290e4ac8211daa96792
Agung Wicaksono <agungwck@landx.id>
2022-12-01 15:16:09 +0700

    feat: load product from database

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
 <script>
+// @ts-nocheck
+
     import ItemCard from "../components/ItemCard.svelte";
     import CampaignCard from "../components/CampaignCard.svelte";
+    import { ref, get } from "firebase/database";
+    import {database} from '../firebase';
+	import Header from "./Header.svelte";
+
+    let dataProduct = [];
+    const getProduct = async () => {
+        const refDb = ref(database, 'products' );
+        get(refDb).then((snapshot) => {
+            const data = snapshot.val();
+            dataProduct = data;
+        });
+    }
+    getProduct();
 </script>
 
 <section id="campaign-list" class="py-2">
@@ -13,13 +28,9 @@
     </div>
 </section>
 <section class="item-container grid grid-cols-3 gap-2 py-2">
-    <ItemCard />
-    <ItemCard />
-    <ItemCard />
-    <ItemCard />
-    <ItemCard />
-    <ItemCard />
-    <ItemCard />
-    <ItemCard />
-    <ItemCard />
+    {#each dataProduct as product, index}
+        {#if product }
+            <ItemCard {...product} id={index} />
+        {/if}
+    {/each}
 </section>
