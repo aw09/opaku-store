04dacd138ec7b129a4e66123977f1d2efd96f7a3
Agung Wicaksono <agungwck@landx.id>
2022-12-02 21:32:30 +0700

    feat: add alias path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$src: path.resolve('./src'),
+			$routes: path.resolve('./src/routes'),
+			$components: path.resolve('./src/components'),
+			$state: path.resolve('./src/routes/state'),
+			$firebase: path.resolve('./src/firebase')
+		}
+	}
 };
 
 export default config;
