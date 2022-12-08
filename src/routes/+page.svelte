a1f1e0361c19d5ae2d85a470b6e7667052f531da
Agung Wicaksono <agungwck@landx.id>
2022-12-08 10:31:04 +0700

    fix: loading state on home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,45 +12,41 @@
         userData = value;
     })
 
-
-    let dataCampaign = [];
-    const getCampaign = async () => {
-        const refDb = ref(database, 'campaign' );
-        get(refDb).then((snapshot) => {
-            const data = snapshot.val();
-            dataCampaign = data;
-        });
+    const fetchData = async (path) => {
+        const refDb = ref(database, path );
+        return (await get(refDb)).val()
     }
+    const campaignPromise = fetchData('campaign');
+    const productPromise = fetchData('products');
 
-    let dataProduct = [];
-    const getProduct = async () => {
-        const refDb = ref(database, 'products' );
-        get(refDb).then((snapshot) => {
-            const data = snapshot.val();
-            dataProduct = data;
-        });
-    }
-
-    getCampaign();
-    getProduct();
 </script>
 
 <head>
     <title>Home</title>
 </head>
-<section id="campaign-list" class="py-2 overflow-x-scroll">
-    <div class="flex gap-2 relative min-w-max overflow-x-scroll pb-2">
-    {#each dataCampaign as campaign, index}
-        {#if campaign }
+
+{#await campaignPromise}
+    <span>Loading campaign data...</span>
+{:then dataCampaign} 
+    <section id="campaign-list" class="py-2 overflow-x-scroll">
+        <div class="flex gap-2 relative min-w-max overflow-x-scroll pb-2">
+            {#each dataCampaign as campaign, index}
+            {#if campaign }
             <CampaignCard {...campaign} id={index} />
-        {/if}
-    {/each}
-    </div>
-</section>
-<section class="item-container grid gap-2 py-2 md:grid-cols-3 grid-cols-2">
-    {#each dataProduct as product, index}
-        {#if product }
-            <ItemCard {...product} id={index} />
-        {/if}
-    {/each}
-</section>
+            {/if}
+            {/each}
+        </div>
+    </section>
+{/await}
+
+{#await productPromise}
+    <span>Loading products data...</span>
+{:then dataProduct} 
+    <section class="item-container grid gap-2 py-2 md:grid-cols-3 grid-cols-2">
+        {#each dataProduct as product, index}
+            {#if product }
+                <ItemCard {...product} id={index} />
+            {/if}
+        {/each}
+    </section>
+{/await}
